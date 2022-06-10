d8329982e8a283087e7a8686d59b62a47c7e282a
chuhoon <khchu1220@naver.com>
2022-06-10 21:53:08 +0900

    final

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 const elcont = document.querySelector('#app');
-
-let userMoney = 100000;
-let restMoney = 0;
-let totalPrice = 0;
-
 const arrsrc = [
   './images/Original_Cola.png',
   './images/Violet_Cola.png',
@@ -12,7 +7,6 @@ const arrsrc = [
   './images/Green_Cola.png',
   './images/Orange_Cola.png',
 ];
-
 const arrColaName = [
   'Original_Cola',
   'Violet_Cola',
@@ -22,149 +16,155 @@ const arrColaName = [
   'Orange_Cola',
 ];
 
-const elh1 = document.createElement('h1');
-elh1.classList.add('title');
+let userMoney = 100000;
+let restMoney = 0;
+let totalPrice = 0;
 
-const elimg = document.createElement('img');
-elimg.classList.add('title-logo');
-elimg.src = './images/Logo_img.png';
+function paint() {
+  const elh1 = document.createElement('h1');
+  elh1.classList.add('title');
 
-const elvendingmachine = document.createElement('section');
-elvendingmachine.classList.add('vending-machine');
+  const elimg = document.createElement('img');
+  elimg.classList.add('title-logo');
+  elimg.src = './images/Logo_img.png';
 
-// 왼쪽
-const elcontLeft = document.createElement('article');
-elcontLeft.classList.add('cont-left');
+  const elvendingmachine = document.createElement('section');
+  elvendingmachine.classList.add('vending-machine');
 
-const elUl = document.createElement('ul');
-elUl.classList.add('btn-wrap');
+  elcont.appendChild(elh1);
+  elh1.appendChild(elimg);
 
-for (let i = 0; i < 6; i++) {
-  const elColaLi = document.createElement('li');
-  const elColaBtn = document.createElement('button');
-  elColaBtn.classList.add('btn-cola');
-  elColaBtn.classList.add(`${arrColaName[i]}`);
-  const ColaImg = document.createElement('img');
-  ColaImg.src = `${arrsrc[i]}`;
-  const elColaName = document.createElement('span');
-  elColaName.classList.add('cola-name');
-  console.log(elColaName);
-  elColaName.innerText = `${arrColaName[i]}`;
-  const elColaPrice = document.createElement('span');
-  elColaPrice.classList.add('cola-price');
-  elColaPrice.innerText = '1000원';
+  // 왼쪽
+  const elcontLeft = document.createElement('article');
+  elcontLeft.classList.add('cont-left');
 
-  elUl.appendChild(elColaLi);
-  elColaLi.appendChild(elColaBtn);
-  elColaBtn.appendChild(ColaImg);
-  elColaBtn.appendChild(elColaName);
-  elColaBtn.appendChild(elColaPrice);
+  const elUl = document.createElement('ul');
+  elUl.classList.add('btn-wrap');
+
+  for (let i = 0; i < 6; i++) {
+    const elColaLi = document.createElement('li');
+    const elColaBtn = document.createElement('button');
+    elColaBtn.classList.add('btn-cola');
+    elColaBtn.classList.add(`${arrColaName[i]}`);
+    const ColaImg = document.createElement('img');
+    ColaImg.src = `${arrsrc[i]}`;
+    const elColaName = document.createElement('span');
+    elColaName.classList.add('cola-name');
+    console.log(elColaName);
+    elColaName.innerText = `${arrColaName[i]}`;
+    const elColaPrice = document.createElement('span');
+    elColaPrice.classList.add('cola-price');
+    elColaPrice.innerText = '1000원';
+
+    elUl.appendChild(elColaLi);
+    elColaLi.appendChild(elColaBtn);
+    elColaBtn.appendChild(ColaImg);
+    elColaBtn.appendChild(elColaName);
+    elColaBtn.appendChild(elColaPrice);
+  }
+
+  const elBuy = document.createElement('section');
+  elBuy.classList.add('grid-buy');
+
+  const elhidden = document.createElement('h2');
+  elhidden.classList.add('hidden');
+  elhidden.innerText = '구매관련목록';
+
+  const elContBalanceMoney = document.createElement('div');
+  elContBalanceMoney.classList.add('cont-balance-money');
+  elContBalanceMoney.innerText = '잔액 : ';
+
+  const elSpMoney = document.createElement('span');
+  elSpMoney.classList.add('sp-money');
+  elSpMoney.innerText = `${restMoney}원`;
+
+  const BtnReturn = document.createElement('button');
+  BtnReturn.classList.add('btn-return');
+  BtnReturn.innerText = '거스름돈 반환';
+
+  const elInputMoney = document.createElement('input');
+  elInputMoney.classList.add('cont-input-money');
+  elInputMoney.type = 'number';
+  elInputMoney.placeholder = '입금액 입력';
+
+  const btnDeposit = document.createElement('button');
+  btnDeposit.classList.add('btn-deposit');
+  btnDeposit.innerText = '입금';
+
+  const contListBuy = document.createElement('div');
+  contListBuy.classList.add('list-buy');
+
+  const btnIncome = document.createElement('button');
+  btnIncome.classList.add('btn-income');
+  btnIncome.innerText = '획득';
+
+  // 오른쪽
+  const elcontRight = document.createElement('article');
+  elcontRight.classList.add('cont-right');
+
+  // 오른쪽 위
+  const elcontRightHigh = document.createElement('div');
+  elcontRightHigh.classList.add('cont-righthigh');
+
+  const elh2 = document.createElement('h2');
+  elh2.innerText = '소지금';
+  elh2.classList.add('hidden');
+
+  const elcontPossession = document.createElement('div');
+  elcontPossession.innerText = '소지금 : ';
+  elcontPossession.classList.add('cont-possession');
+
+  const elTxtPos = document.createElement('span');
+  elTxtPos.innerText = `${userMoney} 원`;
+  elTxtPos.classList.add('txt-pos');
+
+  // 오른쪽 아래
+  const elcontRightLow = document.createElement('div');
+  elcontRightLow.classList.add('cont-rightlow');
+
+  const elSubTitle = document.createElement('h2');
+  elSubTitle.classList.add('sub-title');
+  elSubTitle.innerText = '획득한 음료';
+
+  const listGetBeverage = document.createElement('div');
+  listGetBeverage.classList.add('list-getbeverage');
+
+  const txtTotalPrice = document.createElement('strong');
+  txtTotalPrice.classList.add('txt-totalprice');
+  txtTotalPrice.innerText = `총금액 : ${totalPrice}`;
+
+  //왼쪽
+  elcont.appendChild(elvendingmachine);
+  elvendingmachine.appendChild(elcontLeft);
+  elcontLeft.appendChild(elUl);
+
+  elcontLeft.appendChild(elBuy);
+  elBuy.appendChild(elhidden);
+  elBuy.appendChild(elContBalanceMoney);
+  elContBalanceMoney.appendChild(elSpMoney);
+
+  elBuy.appendChild(BtnReturn);
+  elBuy.appendChild(elInputMoney);
+  elBuy.appendChild(btnDeposit);
+
+  elBuy.appendChild(contListBuy);
+  elBuy.appendChild(btnIncome);
+
+  // 오른쪽
+  elvendingmachine.appendChild(elcontRight);
+  elcontRight.appendChild(elcontRightHigh);
+  elcontRightHigh.appendChild(elh2);
+  elcontRightHigh.appendChild(elcontPossession);
+  elcontPossession.appendChild(elTxtPos);
+
+  elcontRight.appendChild(elcontRightLow);
+  elcontRightLow.appendChild(elSubTitle);
+  elcontRightLow.appendChild(listGetBeverage);
+  elcontRightLow.appendChild(txtTotalPrice);
 }
 
-const elBuy = document.createElement('section');
-elBuy.classList.add('grid-buy');
-
-const elhidden = document.createElement('h2');
-elhidden.classList.add('hidden');
-elhidden.innerText = '구매관련목록';
-
-const elContBalanceMoney = document.createElement('div');
-elContBalanceMoney.classList.add('cont-balance-money');
-elContBalanceMoney.innerText = '잔액 : ';
-
-const elSpMoney = document.createElement('span');
-elSpMoney.classList.add('sp-money');
-elSpMoney.innerText = `${restMoney}원`;
-
-const elBtnReturn = document.createElement('button');
-elBtnReturn.classList.add('btn-return');
-elBtnReturn.innerText = '거스름돈 반환';
-
-const elInputMoney = document.createElement('input');
-elInputMoney.classList.add('cont-input-money');
-elInputMoney.type = 'number';
-elInputMoney.placeholder = '입금액 입력';
-
-const elBtnDeposit = document.createElement('button');
-elBtnDeposit.classList.add('btn-deposit');
-elBtnDeposit.innerText = '입금';
-
-const elContListBuy = document.createElement('div');
-elContListBuy.classList.add('list-buy');
-
-const elBtnIncome = document.createElement('button');
-elBtnIncome.classList.add('btn-income');
-elBtnIncome.innerText = '획득';
-
-// 오른쪽
-const elcontRight = document.createElement('article');
-elcontRight.classList.add('cont-right');
-
-// 오른쪽 위
-const elcontRightHigh = document.createElement('div');
-elcontRightHigh.classList.add('cont-righthigh');
-
-const elh2 = document.createElement('h2');
-elh2.innerText = '소지금';
-elh2.classList.add('hidden');
-
-const elcontPossession = document.createElement('div');
-elcontPossession.innerText = '소지금 : ';
-elcontPossession.classList.add('cont-possession');
-
-const elTxtPos = document.createElement('span');
-elTxtPos.innerText = `${userMoney} 원`;
-elTxtPos.classList.add('txt-pos');
-
-// 오른쪽 아래
-const elcontRightLow = document.createElement('div');
-elcontRightLow.classList.add('cont-rightlow');
-
-const elSubTitle = document.createElement('h2');
-elSubTitle.classList.add('sub-title');
-elSubTitle.innerText = '획득한 음료';
-
-const elListGetBeverage = document.createElement('div');
-elListGetBeverage.classList.add('list-getbeverage');
-
-const elTotalPrice = document.createElement('strong');
-elTotalPrice.classList.add('txt-totalprice');
-elTotalPrice.innerText = `총금액 : ${totalPrice}`;
-
-// 넣기
-elcont.appendChild(elh1);
-elh1.appendChild(elimg);
-
-//왼쪽
-elcont.appendChild(elvendingmachine);
-elvendingmachine.appendChild(elcontLeft);
-elcontLeft.appendChild(elUl);
-
-elcontLeft.appendChild(elBuy);
-elBuy.appendChild(elhidden);
-elBuy.appendChild(elContBalanceMoney);
-elContBalanceMoney.appendChild(elSpMoney);
-
-elBuy.appendChild(elBtnReturn);
-elBuy.appendChild(elInputMoney);
-elBuy.appendChild(elBtnDeposit);
-
-elBuy.appendChild(elContListBuy);
-elBuy.appendChild(elBtnIncome);
-
-// 오른쪽
-elvendingmachine.appendChild(elcontRight);
-elcontRight.appendChild(elcontRightHigh);
-elcontRightHigh.appendChild(elh2);
-elcontRightHigh.appendChild(elcontPossession);
-elcontPossession.appendChild(elTxtPos);
-
-elcontRight.appendChild(elcontRightLow);
-elcontRightLow.appendChild(elSubTitle);
-elcontRightLow.appendChild(elListGetBeverage);
-elcontRightLow.appendChild(elTotalPrice);
-
 function input() {
+  const elBtnDeposit = document.querySelector('.btn-deposit');
   elBtnDeposit.addEventListener('click', function () {
     let inputMoney = document.querySelector('.cont-input-money').value;
     let elrestMoney = document.querySelector('.sp-money');
@@ -185,6 +185,7 @@ function input() {
 }
 
 function returnMoney() {
+  const elBtnReturn = document.querySelector('.btn-return');
   elBtnReturn.addEventListener('click', function () {
     let elTxtPosMoney = document.querySelector('.txt-pos');
     let elrestMoney = document.querySelector('.sp-money');
@@ -195,22 +196,9 @@ function returnMoney() {
   });
 }
 
-let colaList0 = document.querySelector('.Original_Cola');
-let colaList1 = document.querySelector('.Violet_Cola');
-let colaList2 = document.querySelector('.Yellow_Cola');
-let colaList3 = document.querySelector('.Cool_Cola');
-let colaList4 = document.querySelector('.Green_Cola');
-let colaList5 = document.querySelector('.Orange_Cola');
-
 function clickCola() {
-  let colaList = [
-    colaList0,
-    colaList1,
-    colaList2,
-    colaList3,
-    colaList4,
-    colaList5,
-  ];
+  const elContListBuy = document.querySelector('.list-buy');
+  const colaList = document.querySelectorAll('.btn-cola');
 
   for (let i = 0; i < 6; i++) {
     colaList[i].addEventListener('click', function () {
@@ -236,9 +224,12 @@ function clickCola() {
   }
 }
 
-const elColaBtnList = document.querySelectorAll('.btn-cola');
-
 function getCola() {
+  const elListGetBeverage = document.querySelector('.list-getbeverage');
+  const elTotalPrice = document.querySelector('.txt-totalprice');
+  const elContListBuy = document.querySelector('.list-buy');
+  const elBtnIncome = document.querySelector('.btn-income');
+  const elColaBtnList = document.querySelectorAll('.btn-cola');
   elBtnIncome.addEventListener('click', function () {
     let elBtnGetBeverage = elContListBuy.querySelectorAll('.btn-getbeverage');
     let elrestMoney = document.querySelector('.sp-money');
@@ -270,6 +261,7 @@ function getCola() {
 }
 
 function init() {
+  paint();
   input();
   returnMoney();
   clickCola();
